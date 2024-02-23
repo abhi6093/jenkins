8327b3d72342684eecae9b645f20113a4f1c5878
abhi6093 <abhishekdal05@mail.com>
2024-02-23 16:00:04 +0530

    new-update

--- jenkins-installtion/jenkins.tf
+++ jenkins-installtion/jenkins.tf
@@ -12,13 +12,20 @@ resource "aws_instance" "jenkins_instance" {
   }
 }
  
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        private_key = file("./private.pem")
+        host     = self.public_ip
+    }
+
     provisioner "remote-exec" {
     inline = [
         "sudo apt update",
         "sudo apt install fontconfig openjdk-17-jre",
         "java -version",
-        "sudo wget -O /etc/yum.repos.d/jenkins.repo \
-            https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+        "sudo wget -O /etc/yum.repos.d/jenkins.repo \",
+            "https://pkg.jenkins.io/redhat-stable/jenkins.repo",
         "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
         "sudo dnf upgrade",
         "sudo dnf install fontconfig java-17-openjdk",
