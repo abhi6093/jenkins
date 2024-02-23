a2b87a0bba86a17031930f0755dd16dd2fcb3cbd
abhi6093 <abhishekdal05@mail.com>
2024-02-23 16:26:44 +0530

    final_try

--- jenkins-installtion/jenkins.tf
+++ jenkins-installtion/jenkins.tf
@@ -14,9 +14,6 @@ resource "aws_instance" "jenkins_instance" {
               #!/bin/bash
               sudo apt-get update -y
               sudo apt-get install openjdk-8-jdk -y
-              sudo wget -O /etc/yum.repos.d/jenkins.repo \
-                   https://pkg.jenkins.io/redhat-stable/jenkins.repo
-              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
               sudo apt-get install jenkins -y
               sudo systemctl start jenkins
               sudo systemctl enable jenkins
