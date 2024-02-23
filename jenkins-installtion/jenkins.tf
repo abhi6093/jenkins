8c441e10be687e24f0095ecf0fa5d65b88f3f399
abhi6093 <abhishekdal05@mail.com>
2024-02-23 16:39:01 +0530

    final-update

--- jenkins-installtion/jenkins.tf
+++ jenkins-installtion/jenkins.tf
@@ -13,9 +13,14 @@ resource "aws_instance" "jenkins_instance" {
     user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update -y
-              sudo apt-get install openjdk-8-jdk -y
-              sudo apt-get install jenkins -y
-              sudo systemctl start jenkins
+              sudo dnf install fontconfig java-17-openjdk
+              sudo wget -O /etc/yum.repos.d/jenkins.repo \
+                     https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+              sudo dnf upgrade
+              sudo dnf install jenkins             
+              sudo systemctl daemon-reload 
               sudo systemctl enable jenkins
+              sudo systemctl start jenkins
               EOF
 }
