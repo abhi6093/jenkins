af012a703ceda31a5c478dee97b88f0fa9600f55
abhi6093 <abhishekdal05@mail.com>
2024-02-23 16:07:24 +0530

    final-try

--- jenkins-installtion/jenkins.tf
+++ jenkins-installtion/jenkins.tf
@@ -10,29 +10,15 @@ resource "aws_instance" "jenkins_instance" {
   tags = {
     Name = "jenkins_instance"
   }
+    user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install openjdk-8-jdk -y
+              sudo wget -O /etc/yum.repos.d/jenkins.repo \
+                   https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+              sudo apt-get install jenkins -y
+              sudo systemctl start jenkins
+              sudo systemctl enable jenkins
+              EOF
 }
- 
-    connection {
-        type     = "ssh"
-        user     = "ec2-user"
-        private_key = file("./private.pem")
-        host     = self.public_ip
-    }
-
-    provisioner "remote-exec" {
-    inline = [
-        "sudo apt update",
-        "sudo apt install fontconfig openjdk-17-jre",
-        "java -version",
-        "sudo wget -O /etc/yum.repos.d/jenkins.repo \",
-            "https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-        "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
-        "sudo dnf upgrade",
-        "sudo dnf install fontconfig java-17-openjdk",
-        "sudo dnf install jenkins",
-        "sudo systemctl daemon-reload",
-        "sudo systemctl enable jenkins",
-        "sudo systemctl start jenkins",
-        "sudo systemctl status jenkins",
-        ]
-    }
